00efbe52283c73a35eb5c7b26af24b9669133946
Alondra Borja <alondracorbillonborjaborja14.ab@gmail.com>
2021-08-27 16:39:47 +0800

    added

---
+++ server/model/model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+var schema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    gender: String
+})
+
+const Userdb = mongoose.model('userdb', schema);
+
+module.exports = Userdb;
