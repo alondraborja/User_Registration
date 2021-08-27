b76c5ac0c286c6d91022410b07e3105211d4792f
Alondra Borja <alondracorbillonborjaborja14.ab@gmail.com>
2021-08-27 16:37:09 +0800

    added the path for the database

--- server.js
+++ server.js
@@ -4,13 +4,18 @@ const morgan = require('morgan');
 const bodyparser = require('body-parser');
 const path = require('path'); 
 
+const connectDB = require('./server/database/connection');
+
 const app = express();
 
-dotenv.config({ path:'config.env' })
+dotenv.config({ path:'config.env'})
 const PORT = process.env.PORT || 8080
 
 //log requests
-app.use(morgan('tiny'))
+app.use(morgan('tiny'));
+
+//mongodb connection
+connectDB();
 
 //parse request to body-parser
 app.use(bodyparser.urlencoded({extended:true}))
@@ -23,18 +28,8 @@ app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
 app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
 app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 
-app.get('/', (req, res) => {
-    res.render('index');
-})
-
-app.get('/add-user', (req, res) => {
-    res.render('add_user');
-})
-
-app.get('/update-user', (req, res) => {
-    res.render('update_user');
-})
-
+//load routers
+app.use('/', require('./server/routes/router'));
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
